6cb2dddf60b058150d5952d380ed49274a30aa09
Dhiva-R <workdhiva@gmail.com>
2022-11-04 14:07:45 +0530

    am

--- src/db/conn.js
+++ src/db/conn.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb+srv://dhiva:pWHgcTExxTyPu6DT@cluster0@cluster0.kszv2p0.mongodb.net/?retryWrites=true&w=majority",{
+mongoose.connect("mongodb+srv://dhiva:iKLB742OeVZuAJ4X@cluster0.kszv2p0.mongodb.net/?retryWrites=true&w=majority",{
     useNewUrlParser:true,
     useUnifiedTopology:true
 }).then(()=>
