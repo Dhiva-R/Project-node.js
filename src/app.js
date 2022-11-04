f9f15a390766c1d5ad0f454092753e0dfeaea97a
Dhiva-R <workdhiva@gmail.com>
2022-11-04 16:43:58 +0530

    procfil8

--- src/app.js
+++ src/app.js
@@ -117,6 +117,6 @@ app.post('/login',async(req,res)=>{
 app.get("/",(req,res)=>{
     var result='App is running'
     res.send(result);
-}).listen(app.get('PORT'),function(){
+}).listen(app.get('port'),function(){
     console.log('Server listening on port',app.get('port'));
 })
